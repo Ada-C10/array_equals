5548c591eb804c4d2381273e6efe40528b549e3a
Alice Hsiao <alice.hsiao87@gmail.com>
2018-08-16 20:55:12 -0700

    added array_equals method and used rake to check that tests passed

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,25 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  index = 0
+
+  # Edge Cases
+  if (array1 == nil && array2 == nil) || (array1 == [] && array2 == [])
+    return true
+  elsif array1 == nil || array2 == nil
+    return false
+  end
+
+  # If: Compare # of items in each array
+  # Else: Compare each item individually in array1 and array2
+  if array1.length != array2.length
+    return false
+  else
+    while index < array1.length do
+      return false if array1[index] != array2[index]
+      index += 1
+    end
+  end
+
+  return true
 end
