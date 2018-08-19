a5fcb72b2455aa99fcec1780097bd57962c6d82c
Laura <larmartinez2@gmail.com>
2018-08-19 15:59:31 -0700

    no failures, but 3 errors because of nill

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,15 @@
+require 'pry'
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1.length == array2.length
+      if array1.length != 0 && array1 != nil
+        array1.length.times do |index|
+          return false if array1[index] != array2[index]
+        end
+      end
+    return true
+  else
+    return false
+  end
 end
