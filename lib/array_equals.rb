b8f29a77add577250356d16742c16294268a2f87
Lindsay Terchin <lterchin@gmail.com>
2018-08-20 20:35:28 -0700

    created method to see if two input arrays are the same

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,29 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+
+#define method to see if an array is equal to nil
+def check_nil(array)
+  if array == nil
+    array ||= 0
+  end
+  return array
+end
+
+#define a method to see if one array is equal to another one
 def array_equals(array1, array2)
+  if check_nil(array1) == check_nil(array2)
+    return true
+  elsif
+    check_nil(array1) != check_nil(array2)
+    return false
+  elsif array1.length == array2.length
+    array1.each do |number|
+      number == array2[index] ? true : false
+      index +=1
+    end
+    return true
+  else
+    return false
+  end
   raise NotImplementedError
 end
