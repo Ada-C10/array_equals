05b25da2f9e3d1713c54a22609ba0ebe87ec87cf
Pushpa Agr <pushpaagr108@gmail.com>
2018-08-21 23:35:37 -0700

    code condensed and removed repeative code, all tests passed

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,10 +1,7 @@
-require 'pry'
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 
-
 def array_equals(array1, array2)
-
 
   if array1 == nil && array2 == nil
     return true
@@ -12,50 +9,31 @@ def array_equals(array1, array2)
     return false
   elsif array1 != nil && array2 == nil
     return false
+  elsif array1 == [] && array2 == []
+    return true
   end
-
 
   array1_length = 0
   array1.each do |element|
     array1_length = array1_length + 1
   end
 
-
   array2_length = 0
   array2.each do |element|
     array2_length = array2_length + 1
   end
 
-  index1 = 0
-  array1_length.times do
-    if (array1_length == array2_length) && (array1[index1] == array2[index1]) && (array2[index1] == array1[index1])
-      index1 = index1 + 1
+  if array1_length == array2_length
+    index1 = 0
+    array1_length.times do
+      if  (array1[index1] == array2[index1])
+        index1 = index1 + 1
+      else
+        return false
+      end
     end
-  end
-
-  index2 = 0
-  array2_length.times do
-    if (array2_length == array1_length) && (array2[index2] == array1[index2]) && (array2[index2] == array1[index2])
-      index2 = index2 + 1
-    end
-  end
-
-  # puts index1
-  # puts array1_length
-  # puts index2
-  # puts array2_length
-
-
-  if (index1 == array2_length) && (index2 == array1_length)
     return true
   else
     return false
   end
 end
-#
-# #
-# array1 = [10, 20, 30, 40, 50, 60]
-# array2 = []
-#
-# a = array_equals(array1, array2)
-# puts a
