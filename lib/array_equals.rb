564b667006182719e3e3f52c382da397940fe986
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-27 11:15:13 -0700

    Write method array_equals with passing tests

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,3 @@
-# Determines if the two input arrays have the same count of elements
-# and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  return array1 == array2
 end
