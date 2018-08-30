f569179863b0b7ca1c1dc849ca89bbc40f3fbf01
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-29 17:24:56 -0700

    Write method properly for array_equals

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,3 +1,19 @@
 def array_equals(array1, array2)
-  return array1 == array2
+  if array1 == nil && array2 == nil
+    return true
+  elsif array1 == nil || array2 == nil
+    return false
+  elsif
+    array1.length != array2.length
+    return false
+  end
+
+  i = 0
+  while i < array1.length do
+    if array1[i] != array2[i]
+      return false
+    end
+    i += 1
+  end
+  return true
 end
