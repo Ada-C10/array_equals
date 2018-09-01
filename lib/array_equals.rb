b3b9634cba6aa46618750e7cbce7768028921fe7
sabine <drsabinethomas@gmail.com>
2018-08-31 21:16:13 -0700

    Eureka

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,16 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1 == array2
+    return true
+  else
+    return false
+  end
+
+  if array1.hash.length == array2.hash.length
+    return true
+  elsif array1[0] == array2[0]
+    return true
+  end
+
 end
