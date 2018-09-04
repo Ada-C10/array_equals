b4de0bb038fcadcfb27fa90bb4ca40dce603a17a
KatherineJF <kjfitzpat@hotmail.com>
2018-09-03 20:23:34 -0700

    need to add check for same value

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,21 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+require 'pry'
+
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1.nil? && !array2.nil? || !array1.nil? && array2.nil?  || array1&.size != array2&.size
+    return false
+
+  elsif array1.nil? && array2.nil?
+    return true
+
+
+    #if array1&.length == array2&.length
+    array1.each do |n|
+      i = array2.index(n)
+      array2.delete_at(i) if i
+    end
+  end
+  array2.any?
+  return true
 end
